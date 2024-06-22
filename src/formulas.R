db7268f96053ee11129b6e6a63975ee3c4b4042a
Salgado Uralde <inaki.salgado-uralde@capgemini.com>
2024-06-22 18:01:09 +0200

    formulas.R actualizado + cambios packages.R

--- src/formulas.R
+++ src/formulas.R
@@ -6,13 +6,14 @@ calcular_tamano_muestral_media <-
     if (is.null(nivel_confianza)) {
       # ERROR DE MUESTREO
       n0 <- var / e ^ 2
+      
       if (metodo == "MAS" && !is.null(N)) {
         n <- n0 / (1 + (n0 / N))
         #tamaño muestral m.a.s. y error muestreo
-        salida <- list(m.a.s. = ceiling(n))
+        salida <- n
       } else {
         #tamaño muestral m.a.c.r. y error muestreo
-        salida <- list(m.a.c.r. = ceiling(n0))
+        salida <- n0
       }
     } else {
       # ERROR MAXIMO ADMISIBLE CON COEFICIENTE DE CONFIANZA FIJADO
@@ -27,7 +28,7 @@ calcular_tamano_muestral_media <-
         n0 <- n01
       }
       else {
-        n0 <- n02
+        n0 <- min(30, n02)
       }
       
       if (metodo == "MAS" && !is.null(N)) {
@@ -35,17 +36,18 @@ calcular_tamano_muestral_media <-
         n1 <- n01 / (1 + (n01 / N))
         #tamaño muestral m.a.s. y con des. Tchebychev
         n2 <- n02 / (1 + (n02 / N))
+        
         if (n1 >= 100) {
-          salida <- list(m.a.s. = ceiling(n1))
+          salida <- n1
         }
         else {
-          salida <- list(m.a.s. = ceiling(n2))
+          salida <- min(100, n2)
         }
       } else {
-        salida <- list(m.a.c.r. = ceiling(n0))
+        salida <- n0
       }
     }
-    return(salida)
+    return(ceiling(salida))
   }
 
 # Función para calcular el tamaño muestral para una PROPORCION
@@ -54,13 +56,14 @@ calcular_tamano_muestral_proporcion <-
     if (is.null(nivel_confianza)) {
       # ERROR DE MUESTREO
       n0 <- P * (1 - P) / e ^ 2
+      
       if (metodo == "MAS" && !is.null(N)) {
         n <- n0 / (1 + ((n0 - 1) / N))
         #tamaño muestral m.a.s. y error muestreo
-        salida <- list(m.a.s. = ceiling(n))
+        salida <- n
       } else {
         #tamaño muestral m.a.c.r. y error muestreo
-        salida <- list(m.a.c.r. = ceiling(n0))
+        salida <- n0
       }
     } else {
       # ERROR MAXIMO ADMISIBLE CON COEFICIENTE DE CONFIANZA FIJADO
@@ -75,22 +78,24 @@ calcular_tamano_muestral_proporcion <-
         n0 <- n01
       }
       else {
-        n0 <- n02
+        n0 <- min(30, n02)
       }
+      
       if (metodo == "MAS" && !is.null(N)) {
         #tamaño muestral m.a.s. y suponiendo normalidad
         n1 <- n01 / (1 + ((n01 - 1) / N))
         #tamaño muestral m.a.s. y con des. Tchebychev
         n2 <- n02 / (1 + ((n02 - 1) / N))
+        
         if (N >= 60 && n2 / N <= 0.1) {
-          salida <- list(m.a.s. = ceiling(n1))
+          salida <- n1
         }
         else {
-          salida <- list(m.a.s. = ceiling(n2))
+          salida <- n2
         }
       } else {
-        salida <- list(m.a.c.r. = ceiling(n0))
+        salida <- n0
       }
     }
-    return(salida)
+    return(ceiling(salida))
   }
--- src/packages.R
+++ src/packages.R
@@ -1,10 +1,25 @@
 # src/packages.R
 
 # Cargar librerías
-library(shiny)
-library(shinydashboard)
-library(shinyFeedback)
-library(shinyBS)
+#library(shiny)
+#library(shinydashboard)
+#library(shinyFeedback)
+#library(shinyBS)
+
+# Install and load packages
+list.of.packages <- c("shiny",
+                      "shinydashboard",
+                      "shinyFeedback",
+                      "shinyBS")
+new.packages <- list.of.packages[!(list.of.packages %in%
+                                     installed.packages()[, "Package"])]
+if (length(new.packages)) {
+  install.packages(new.packages)
+}
+lapply(list.of.packages,
+       function(pkg) {
+         library(pkg, character.only = TRUE)
+       })
 
 # Cargar archivos de configuración
 source("formulas.R")
